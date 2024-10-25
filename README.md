8b17fbc4c47384a806c229ee2d0477fc2227cbff
ArtemKot4 <artemon4xxl@gmail.com>
2024-10-25 11:23:54 +0300

    Infinite Forest Java

---
+++ README.md
@@ -0,0 +1,3 @@
+# infinite forest java
+
+ infinite forest - mod for inner core 1.16.200
---
+++ java/src/com/artemkot4/infinite_forest/InfiniteForest.java
@@ -0,0 +1,22 @@
+package com.artemkot4.infinite_forest;
+
+import com.zhekasmirnov.apparatus.modloader.LegacyInnerCoreMod;
+import ru.koshakmine.icstd.modloader.Mod;
+import ru.koshakmine.icstd.modloader.ObjectFactory;
+import ru.koshakmine.icstd.utils.TranslationLoader;
+
+public class InfiniteForest extends Mod {
+    public static String WINDOWS;
+
+    public InfiniteForest(String dir, LegacyInnerCoreMod mod) {
+        super(dir, mod);
+
+        WINDOWS = dir + "windows/";
+    }
+
+    @Override
+    public void runMod(ObjectFactory objectFactory) {
+        TranslationLoader.loadTranslation(getDir()+"lang", "en");
+
+    }
+}
