bf3ba1012b6de1552703e208d6863e8bda2ba38e
ArtemKot4 <artemon4xxl@gmail.com>
2024-10-26 15:06:09 +0300

    Теперь предметы и блоки, унаследованные от F (Forest) вариантов будут в креативе по умолчанию

--- java/src/com/artemkot4/infinite_forest/utils/FBlock.java
+++ java/src/com/artemkot4/infinite_forest/utils/FBlock.java
@@ -1,11 +1,12 @@
 package com.artemkot4.infinite_forest.utils;
 
 import ru.koshakmine.icstd.block.Block;
+import ru.koshakmine.icstd.type.CreativeCategory;
 
 public class FBlock extends Block {
     private final String id;
     private final String[] textures;
-
+    private CreativeCategory category;
 
     public FBlock(String id, String[] textures) {
           this.id = id;
@@ -30,5 +31,15 @@ public class FBlock extends Block {
     @Override
     public String getName() {
         return "block.infinite_forest." + id;
+    };
+
+    public FBlock setCreativeCategory(CreativeCategory category) {
+        this.category = category;
+        return this;
+    }
+
+    @Override
+    public CreativeCategory getCreativeCategory() {
+        return category != null ? category : CreativeCategory.MATERIAL;
     }
 }
--- java/src/com/artemkot4/infinite_forest/utils/FItem.java
+++ java/src/com/artemkot4/infinite_forest/utils/FItem.java
@@ -1,6 +1,7 @@
 package com.artemkot4.infinite_forest.utils;
 
 import ru.koshakmine.icstd.item.Item;
+import ru.koshakmine.icstd.type.CreativeCategory;
 import ru.koshakmine.icstd.type.common.Texture;
 import ru.koshakmine.icstd.item.event.IOverrideIcon;
 
@@ -13,6 +14,7 @@ public class FItem extends Item {
     private final String id;
     private final Texture texture;
     private final int stack;
+    private CreativeCategory category;
 
     public FItem(String id, Texture texture, int stack) {
         this.id = id;
@@ -43,16 +45,28 @@ public class FItem extends Item {
         return "item.infinite_forest." + id;
     }
 
-    public void setOnHand(IItemHand lambda) {
+    public FItem setOnHand(IItemHand lambda) {
          handFunctions.put(this.getNumId(), lambda);
+         return this;
     };
 
-    public void setTooltype(IOverrideIcon lambda) {
+    public FItem setTooltype(IOverrideIcon lambda) {
         Item.registerOverrideIcon(this.getNumId(), lambda);
+        return this;
     };
 
 
     public int getMaxStack() {
         return this.stack;
     };
+
+    public FItem setCreativeCategory(CreativeCategory category) {
+        this.category = category;
+        return this;
+    }
+
+    @Override
+    public CreativeCategory getCreativeCategory() {
+        return category != null ? category : CreativeCategory.TOOLS;
+    }
 }
