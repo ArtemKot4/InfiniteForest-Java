42ef7b8a9f31a25353acf2c4459713baf711f252
ArtemKot4 <artemon4xxl@gmail.com>
2024-10-26 12:14:12 +0300

    created and updated Forest.java

---
+++ java/src/com/artemkot4/infinite_forest/dimension/Forest.java
@@ -0,0 +1,18 @@
+package com.artemkot4.infinite_forest.dimension;
+
+
+import com.zhekasmirnov.innercore.api.dimensions.CustomDimension;
+import com.zhekasmirnov.innercore.api.mod.adaptedscript.AdaptedScriptAPI.Dimensions;
+
+public class Forest {
+
+    public static CustomDimension dimension = new Dimensions.CustomDimension("infinite_forest", 75)
+            .setSkyColor(21 / 255, 96 / 255, 189 / 255)
+            .setFogColor(0 / 255, 128 / 255, 0 / 255)
+            .setHasSkyLight(false);
+
+    public static void addLayer() {
+
+    }
+
+}
